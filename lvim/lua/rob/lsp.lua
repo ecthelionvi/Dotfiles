4d9d2cced1f6319749d3085691af41fb79a36a48
ecthelionvi <rsears2@huskers.unl.edu>
2023-09-11 11:59:27 -0500

    Refactoring

--- lvim/lua/rob/lsp.lua
+++ lvim/lua/rob/lsp.lua
@@ -7,9 +7,19 @@ lvim.lsp.on_attach_callback = function(client, _)
   client.server_capabilities.semanticTokensProvider = nil
 end
 
+-- TSServer
+require('lspconfig').tsserver.setup({
+  init_options = {
+    preferences = {
+      disableSuggestions = true,
+    },
+  },
+})
+
 -- Keymaps
 lvim.lsp.buffer_mappings.normal_mode['K'] = nil
 lvim.lsp.buffer_mappings.normal_mode["gl"] = nil
 lvim.lsp.buffer_mappings.normal_mode['gK'] = { vim.lsp.buf.hover, "Show hover" }
 
 return M
+

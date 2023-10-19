ef2510ada800f4e983451d65be09c3e24fcf090b
Robert Sears <rsears2@huskers.unl.edu>
2023-10-19 15:07:04 -0500

    Update telescope.lua

--- lvim/lua/rob/telescope.lua
+++ lvim/lua/rob/telescope.lua
@@ -2,17 +2,48 @@
 
 local M = {}
 
-lvim.builtin.telescope.theme = nil
-lvim.builtin.telescope.defaults.path_display = { "tail" }
+lvim.builtin.telescope.defaults.path_display = { "truncate" }
 lvim.builtin.telescope.on_config_done = function(telescope)
   pcall(telescope.load_extension, "yank_history")
 end
-lvim.builtin.telescope.defaults.layout_config = { preview_width = 59 }
-lvim.builtin.telescope.defaults.file_ignore_patterns = {
-  ".git", ".npm", ".ssh", ".cups", ".cache", ".cargo", ".pyenv", ".vscode",
-  ".rustup", ".android", ".DS_Store", ".localized", "mason", "Caches",
-  "Movies", "Library", "ftplugin", "sessions", "Reminders", "LaunchAgents",
-  "node_modules", "nvim-lspconfig", "Mobile Documents", "com.apple.ProtectedCloudStorage",
+
+
+function M.construct_command_fd(base_command, exclusions)
+  local command = vim.deepcopy(base_command)
+  for _, exclusion in ipairs(exclusions) do
+    table.insert(command, '--exclude')
+    table.insert(command, exclusion)
+  end
+  return command
+end
+
+function M.construct_command_rg(base_command, exclusions)
+  local command = vim.deepcopy(base_command)
+  for _, exclusion in ipairs(exclusions) do
+    table.insert(command, '--glob')
+    table.insert(command, '!' .. exclusion .. '/')
+  end
+  return command
+end
+
+M.base_exclusions = {
+  'node_modules', '.git', '.fig', 'Library', 'fish',
+  '__pycache__', '.cache', '.zsh_sessions', '.DS_Store',
+  '.localized', '.cursor', 'cursor_tutor', '.vscode', '.viminfo',
+  '.rustup', '.nox', '.npm', '.python_history', '.coverage',
+  '.gitignore', 'Movies', 'Music', 'Pictures', 'Applications (Parallels)',
+  '.cargo', '.cups', '.ssh', 'Parallels', '.Trash', '.hushlogin',
+  '.pytest_cache', '.dat', 'mason', '.fig.dotfiles.bak'
 }
+
+lvim.builtin.telescope.pickers.find_files.find_command = M.construct_command_fd(
+  { 'fd', '--type', 'f', '--hidden' },
+  M.base_exclusions
+)
+
+lvim.builtin.telescope.defaults.vimgrep_arguments = M.construct_command_rg(
+  { 'rg', '--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case', '--hidden' },
+  M.base_exclusions
+)
 
 return M

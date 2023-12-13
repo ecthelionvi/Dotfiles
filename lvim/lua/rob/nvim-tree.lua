7517e424624dbc1dfbf29fc12e6a53e7bcc3e8ea
Robert Sears <rsears2@huskers.unl.edu>
2023-12-13 17:07:36 -0600

    Update nvim-tree.lua

--- lvim/lua/rob/nvim-tree.lua
+++ lvim/lua/rob/nvim-tree.lua
@@ -4,7 +4,9 @@ local M = {}
 
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.filters.dotfiles = true
+lvim.builtin.nvimtree.setup.view = { adaptive_size = true }
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 lvim.builtin.nvimtree.setup.update_focused_file.ignore_list = { "toggleterm", "terminal" }
+
 
 return M

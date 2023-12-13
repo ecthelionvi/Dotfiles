aeeeec5c0efb55bcdd33785696b60dd3e717e3d5
Robert Sears <rsears2@huskers.unl.edu>
2023-12-13 17:07:01 -0600

    Update lsp.lua

--- lvim/lua/rob/lsp.lua
+++ lvim/lua/rob/lsp.lua
@@ -1,4 +1,4 @@
--- |||||||||||||||||||||||||||||||||||| LSP ||||||||||||||||||||||||||||||||||| --
+-- &&|||||||||||||||||||||||||||||||||| LSP ||||||||||||||||||||||||||||||||||| --
 
 local M = {}
 
@@ -16,10 +16,21 @@ require('lspconfig').tsserver.setup({
   },
 })
 
+require('lspconfig').pyright.setup({
+  settings = {
+    python = {
+      analysis = {
+        diagnosticSeverityOverrides = {
+          ["reportGeneralTypeIssues"] = "none",
+        },
+      },
+    },
+  },
+})
+
 -- Keymaps
 lvim.lsp.buffer_mappings.normal_mode['K'] = nil
 lvim.lsp.buffer_mappings.normal_mode["gl"] = nil
 lvim.lsp.buffer_mappings.normal_mode['gK'] = { vim.lsp.buf.hover, "Show hover" }
 
 return M
-

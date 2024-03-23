3388c1d2f305d79b814dc3273c0a99fa7fcba404
ecthelionvi <rsears2@huskers.unl.edu>
2024-03-23 17:11:04 -0500

    ...

--- scripts/clean_DS.py
+++ scripts/clean_DS.py
@@ -1,18 +1,8 @@
 import os
-import fnmatch
-import itertools
-import threading
-import time
 import sys
+import fnmatch
+from halo import Halo  # Import Halo for the spinner
 
-# Spinner
-def spinner():
-    spinner = itertools.cycle(['-', '/', '|', '\\'])
-    while True:
-        sys.stdout.write(next(spinner))   # write the next character
-        sys.stdout.flush()                # flush stdout buffer (actual character display)
-        sys.stdout.write('\b')            # erase the last written char
-        time.sleep(0.1)                   # short delay before the next update
 
 def find_and_delete_files(pattern, root_dir):
     deleted_files_count = 0
@@ -22,22 +12,20 @@ def find_and_delete_files(pattern, root_dir):
             try:
                 os.remove(file_path)
                 deleted_files_count += 1
-                # print(f"\rDeleted {file_path}")
             except OSError as e:
                 print(f"\rError deleting {file_path}: {e}")
     return deleted_files_count
+
 
 if __name__ == "__main__":
     pattern = ".DS_Store"
     root_dir = os.path.expanduser("~")  # Start search from the user's home directory
 
-    # Start spinner in a separate thread
-    spinner_thread = threading.Thread(target=spinner, daemon=True)
-    spinner_thread.start()
+    spinner = Halo(
+        text="Deleting .DS_Store Files", spinner="dots"
+    )  # Initialize Halo spinner
+    spinner.start()  # Start the spinner
 
     deleted_files_count = find_and_delete_files(pattern, root_dir)
 
-    # Stop spinner
-    spinner_thread.join(timeout=0.1)
-    sys.stdout.write('\b \b')  # Clear the spinner character
-    print(f"Deleted {deleted_files_count} Files")
+    spinner.stop()  # Stop the spinner when done

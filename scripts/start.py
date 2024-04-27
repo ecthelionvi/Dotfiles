64062fe3a24ef7ff62ffdea7160894e5871bffca
ecthelionvi <rsears2@huskers.unl.edu>
2024-04-27 12:00:38 -0500

    ...

--- scripts/start.py
+++ scripts/start.py
@@ -1,6 +1,7 @@
 import json
 import subprocess
 import os
+import argparse
 from pathlib import Path
 
 
@@ -14,19 +15,30 @@ def find_file_in_tree(start_path, filename):
     return None
 
 
-def run_command_in_directory(command, directory):
+def run_command_in_directory(command, directory, new_tab=False):
     """Runs the specified command in the given directory."""
-    apple_script = f"""
-    tell application "iTerm"
-        tell current window
-            create tab with default profile
+    if new_tab:
+        apple_script = f"""
+        tell application "iTerm"
+            tell current window
+                create tab with default profile
+                tell the current session
+                    write text "clear"
+                    write text "cd '{directory}' && {command}"
+                end tell
+            end tell
+        end tell
+        """
+    else:
+        apple_script = f"""
+        tell application "iTerm"
             tell the current session
                 write text "clear"
                 write text "cd '{directory}' && {command}"
             end tell
         end tell
-    end tell
-    """
+        """
+
     subprocess.run(["osascript", "-e", apple_script], check=True)
 
 
@@ -41,6 +53,12 @@ def find_node_entry_file(directory):
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Run project commands.")
+    parser.add_argument(
+        "-t", "--new-tab", action="store_true", help="Open command in a new tab"
+    )
+    args = parser.parse_args()
+
     project_directory = find_file_in_tree(os.getcwd(), "package.json")
     index_html_directory = find_file_in_tree(os.getcwd(), "index.html")
 
@@ -74,7 +92,7 @@ def main():
         )
 
     if command:
-        run_command_in_directory(command, project_directory)
+        run_command_in_directory(command, project_directory, args.new_tab)
     else:
         print("Unable to start project: No valid 'package.json' or 'index.html' found.")
 

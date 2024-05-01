e40bdec18df59eae3aab88855bcf09d8ae38671e
ecthelionvi <rsears2@huskers.unl.edu>
2024-04-30 19:52:32 -0500

    ...

--- scripts/zip.py
+++ scripts/zip.py
@@ -2,7 +2,6 @@ import subprocess
 import sys
 import os
 
-
 def zip_folder(folder_path):
     if not os.path.isdir(folder_path):
         print(f"The path {folder_path} is not a directory or does not exist.")
@@ -11,16 +10,16 @@ def zip_folder(folder_path):
     folder_name = os.path.basename(folder_path)
     zip_file_name = f"{folder_name}.zip"
 
-    # Running the zip command
+    # Running the zip command with suppressed output
     try:
-        subprocess.check_call(["zip", "-r", zip_file_name, folder_path])
+        with open(os.devnull, 'wb') as devnull:
+            subprocess.check_call(['zip', '-r', zip_file_name, folder_path], stdout=devnull)
         # Green colored success message
-        print(f"\033[92mZipped into {zip_file_name}\033[0m")
+        print(f"\033[92mZipped into\033[0m {zip_file_name}")
     except subprocess.CalledProcessError as e:
         print(f"Error zipping folder: {e}")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:

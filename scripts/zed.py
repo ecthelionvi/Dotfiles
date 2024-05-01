7c2973bccb6012d13b6d2b06153d14702a13a7b0
ecthelionvi <rsears2@huskers.unl.edu>
2024-04-30 20:15:17 -0500

    ...

--- scripts/zed.py
+++ scripts/zed.py
@@ -10,24 +10,24 @@ def main():
 
     filename = sys.argv[1]
 
-    # Use questionary to ask for the target and replacement words
-    target_word = questionary.text("Enter the target word:").ask()
-    replacement_word = questionary.text("Enter the replacement word:").ask()
+    # Use questionary to ask for the target (old name) and replacement (new name) words
+    old_name = questionary.text("Enter the old name:").ask()
+    new_name = questionary.text("Enter the new name:").ask()
 
     # Check if user canceled the input
-    if target_word is None or replacement_word is None:
+    if old_name is None or new_name is None:
         print("Input was canceled. Exiting.")
         sys.exit(1)
 
     # Adjust the sed command for macOS compatibility
     # Use `-i ''` to edit in place without creating a backup file
-    sed_command = f"sed -i '' 's/{target_word}/{replacement_word}/g' {filename}"
+    sed_command = f"sed -i '' 's/{old_name}/{new_name}/g' {filename}"
 
     try:
         subprocess.run(sed_command, check=True, shell=True)
         # Print success message with colored parts
         print(
-            f"\033[92mReplaced\033[0m \033[94m{target_word}\033[0m \033[92mwith\033[0m \033[93m{replacement_word}\033[0m"
+            f"\033[92mRenamed\033[0m \033[93m{old_name}\033[0m \033[92mto\033[0m \033[91m{new_name}\033[0m"
         )
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while trying to replace the word: {e}")

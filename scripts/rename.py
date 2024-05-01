7da5b4223c85f31462b7bea5b0f22364e5146fa7
ecthelionvi <rsears2@huskers.unl.edu>
2024-04-30 19:59:34 -0500

    ...

--- scripts/rename.py
+++ scripts/rename.py
@@ -5,15 +5,15 @@ import sys
 def rename_file(old_name, new_name):
     try:
         os.rename(old_name, new_name)
-        # Green for the words "Renamed" and "to", cyan for old_name, yellow for new_name
+        # Green for the words "Renamed" and "to", yellow for old_name, red for new_name
         print(
-            f"\033[92mRenamed\033[0m \033[96m{old_name}\033[0m \033[92mto\033[0m \033[93m{new_name}\033[0m"
+            f"\033[92mRenamed\033[0m \033[93m{old_name}\033[0m \033[92mto\033[0m \033[91m{new_name}\033[0m"
         )
     except FileNotFoundError:
-        print(f"File not found: \033[96m{old_name}\033[0m")
+        print(f"File not found: \033[93m{old_name}\033[0m")
     except OSError as e:
         print(
-            f"Error renaming \033[96m{old_name}\033[0m to \033[93m{new_name}\033[0m: {e}"
+            f"Error renaming \033[93m{old_name}\033[0m to \033[91m{new_name}\033[0m: {e}"
         )
 
 

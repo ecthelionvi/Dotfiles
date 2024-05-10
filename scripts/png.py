71164ed000f2d05d1681fe3a0ac45686798ade1f
ecthelionvi <rsears2@huskers.unl.edu>
2024-05-09 20:08:25 -0500

    ...

--- scripts/png.py
+++ scripts/png.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 from PIL import Image
 
 RESET_TEXT = "\033[0m"
@@ -30,16 +31,13 @@ def convert_all_to_png():
         ]
         converted_files = []
         invalid_files = []
-
         for file in image_files:
             file_path = os.path.join(current_dir, file)
             if convert_to_png(file_path):
                 converted_files.append(f"{YELLOW_TEXT}{file}{RESET_TEXT}")
             else:
                 invalid_files.append(file)
-
         print_conversion_message(converted_files, invalid_files)
-
     except Exception as e:
         print(f"An error occurred: {e}")
 
@@ -48,7 +46,6 @@ def print_conversion_message(converted_files, invalid_files):
     if invalid_files:
         invalid_files_str = " ".join(invalid_files)
         print(f"{invalid_files_str} not converted")
-
     if converted_files:
         print(f"Converted: {' '.join(converted_files)} to PNG")
     elif not invalid_files:
@@ -61,6 +58,7 @@ def print_usage():
 
 
 if __name__ == "__main__":
+    start_time = time.time()
     if len(sys.argv) == 1:
         convert_all_to_png()
     elif len(sys.argv) == 2 and sys.argv[1] == "--all":
@@ -79,3 +77,6 @@ if __name__ == "__main__":
         print_conversion_message(converted_files, invalid_files)
     else:
         print_usage()
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"\nExecution time: {execution_time:.2f} seconds")

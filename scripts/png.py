b2caeae3b154a6274920e60cfb4d6e5c65e9a772
ecthelionvi <rsears2@huskers.unl.edu>
2024-05-09 20:32:54 -0500

    ...

--- scripts/png.py
+++ scripts/png.py
@@ -1,7 +1,7 @@
 import os
 import sys
-import time
 from PIL import Image
+from concurrent.futures import ThreadPoolExecutor
 
 RESET_TEXT = "\033[0m"
 YELLOW_TEXT = "\033[93m"
@@ -10,46 +10,38 @@ YELLOW_TEXT = "\033[93m"
 def convert_to_png(file_path):
     try:
         if not os.path.exists(file_path):
-            return False
+            return None
         image = Image.open(file_path)
-        png_file = os.path.splitext(file_path)[0] + ".png"
+        filename = os.path.basename(file_path)  # Extract the filename
+        png_file = os.path.splitext(filename)[0] + ".png"
         image.save(png_file, "PNG")
         os.remove(file_path)
-        return True
+        return f"{YELLOW_TEXT}{filename}{RESET_TEXT}"  # Return the filename only
     except Exception as e:
-        print(f"Error converting {file_path}: {e}")
-        return False
+        print(f"Error converting {filename}: {e}")
+        return None
+
+
+def convert_files(files):
+    with ThreadPoolExecutor() as executor:
+        results = list(executor.map(convert_to_png, files))
+    converted_files = [file for file in results if file is not None]
+    if converted_files:
+        print(f"Converted: {' '.join(converted_files)} to PNG")
+    else:
+        print("No image files found or converted.")
 
 
 def convert_all_to_png():
-    try:
-        current_dir = os.getcwd()
-        files = os.listdir(current_dir)
-        image_extensions = [".jpg", ".jpeg", ".png", ".bmp", ".gif", ".webp"]
-        image_files = [
-            f for f in files if os.path.splitext(f)[1].lower() in image_extensions
-        ]
-        converted_files = []
-        invalid_files = []
-        for file in image_files:
-            file_path = os.path.join(current_dir, file)
-            if convert_to_png(file_path):
-                converted_files.append(f"{YELLOW_TEXT}{file}{RESET_TEXT}")
-            else:
-                invalid_files.append(file)
-        print_conversion_message(converted_files, invalid_files)
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-
-def print_conversion_message(converted_files, invalid_files):
-    if invalid_files:
-        invalid_files_str = " ".join(invalid_files)
-        print(f"{invalid_files_str} not converted")
-    if converted_files:
-        print(f"Converted: {' '.join(converted_files)} to PNG")
-    elif not invalid_files:
-        print("No image files found")
+    current_dir = os.getcwd()
+    files = os.listdir(current_dir)
+    image_extensions = [".jpg", ".jpeg", ".png", ".bmp", ".gif", ".webp"]
+    image_files = [
+        os.path.join(current_dir, f)
+        for f in files
+        if os.path.splitext(f)[1].lower() in image_extensions
+    ]
+    convert_files(image_files)
 
 
 def print_usage():
@@ -58,25 +50,15 @@ def print_usage():
 
 
 if __name__ == "__main__":
-    start_time = time.time()
-    if len(sys.argv) == 1:
+    if len(sys.argv) == 1 or (len(sys.argv) == 2 and sys.argv[1] == "--all"):
         convert_all_to_png()
-    elif len(sys.argv) == 2 and sys.argv[1] == "--all":
-        convert_all_to_png()
-    elif len(sys.argv) >= 2:
-        converted_files = []
-        invalid_files = []
-        for file_path in sys.argv[1:]:
-            if file_path == "--all":
-                print_usage()
-                sys.exit(1)
-            if convert_to_png(file_path):
-                converted_files.append(f"{YELLOW_TEXT}{file_path}{RESET_TEXT}")
-            else:
-                invalid_files.append(file_path)
-        print_conversion_message(converted_files, invalid_files)
+    elif len(sys.argv) > 1:
+        file_paths = [
+            os.path.join(os.getcwd(), f) for f in sys.argv[1:] if not f.startswith("--")
+        ]
+        if file_paths:
+            convert_files(file_paths)
+        else:
+            print_usage()
     else:
         print_usage()
-    end_time = time.time()
-    execution_time = end_time - start_time
-    print(f"\nExecution time: {execution_time:.2f} seconds")
